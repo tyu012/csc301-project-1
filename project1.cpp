30c0663509007c69e3a54033f328b8403e018238
Medhashree Adhikari <adhikari3@grinnell.edu>
2025-09-24 16:21:02 -0500

    Edited function comments

--- project1.cpp
+++ project1.cpp
@@ -12,36 +12,51 @@
 
 using namespace std;
 
-/*
+/**
  * selectionSort
+ * Input: 
+ * Output: 
+ * The input array...
  */
 void selectionSort(vector<double> &arrayToSort) {
     return;
 }
 
-/*
+/** 
  * insertionSort
+ * Input: 
+ * Output: 
+ * The input array...
  */
 void insertionSort(vector<double> &arrayToSort) {
     return;
 }
 
-/*
+/** 
  * bubbleSort
+ * Input: 
+ * Output: 
+ * The input array...
  */
 void bubbleSort(vector<double> &arrayToSort) {
     return;
 }
 
-/*
+/** 
  * mergeSort
+ * Input: 
+ * Output: 
+ * The input array...
  */
 void mergeSort(vector<double> &arrayToSort) {
     return;
 }
 
-/*
+/** 
  * quickSortHelper
+ * Input: 
+ * Output: 
+ * The input array...
  * 
  * Note that i is inclusive (i.e., is the first element of subarray).
  * Note that j is not inclusive (i.e., off the end of the subarray).
@@ -51,8 +66,11 @@ void quickSortHelper(vector<double> &arrayToSort, int i, int j) {
     return;
 }
 
-/*
+/** 
  * quickSort
+ * Input: 
+ * Output: 
+ * The input array...
  */
 void quickSort(vector<double> &arrayToSort) {
     quickSortHelper(arrayToSort, 0, arrayToSort.size());
