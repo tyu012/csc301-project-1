9fb348b5fdebd1ac97de2cbfdd8d40e4b9bbf0da
Tim Yu <yuyu.tim@gmail.com>
2025-09-24 16:12:49 -0500

    initial commit

---
+++ project1.cpp
@@ -0,0 +1,60 @@
+/*
+ * CSC-301
+ * project1.cpp
+ * Fall 2025
+ *
+ * Partner 1:
+ * Partner 2:
+ * Date:
+ */
+
+#include "project1.hpp"
+
+using namespace std;
+
+/*
+ * selectionSort
+ */
+void selectionSort(vector<double> &arrayToSort) {
+    return;
+}
+
+/*
+ * insertionSort
+ */
+void insertionSort(vector<double> &arrayToSort) {
+    return;
+}
+
+/*
+ * bubbleSort
+ */
+void bubbleSort(vector<double> &arrayToSort) {
+    return;
+}
+
+/*
+ * mergeSort
+ */
+void mergeSort(vector<double> &arrayToSort) {
+    return;
+}
+
+/*
+ * quickSortHelper
+ * 
+ * Note that i is inclusive (i.e., is the first element of subarray).
+ * Note that j is not inclusive (i.e., off the end of the subarray).
+ * 
+ */
+void quickSortHelper(vector<double> &arrayToSort, int i, int j) {
+    return;
+}
+
+/*
+ * quickSort
+ */
+void quickSort(vector<double> &arrayToSort) {
+    quickSortHelper(arrayToSort, 0, arrayToSort.size());
+    return;
+}
