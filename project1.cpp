79f7e98bb2a4a40ac090d100bfd7c733daf0a372
medhashreea <adhikari3@grinnell.edu>
2025-09-26 19:45:31 -0500

    partially complete merge sort

--- project1.cpp
+++ project1.cpp
@@ -85,33 +85,111 @@ void bubbleSort(vector<double> &arrayToSort) {
 
 /** 
  * mergeSort
- * Input: 
- * Output: 
- * The input array...
- */
+ * Input: vector<double> (reference)
+ * Output: none
+ * Side effect: This function will recursively split and sort the array, then
+ * merge it. It will result in the input array being sorted.
+  */
 void mergeSort(vector<double> &arrayToSort) {
-    return;
+    if (arrayToSort.size() <= 1) {
+        return;
+    }
+
+    int mid_idx = arrayToSort.size()/2;
+
+    vector<double> left(arrayToSort.begin(), arrayToSort.begin() + mid_idx);
+    vector<double> right(arrayToSort.begin() + mid_idx, arrayToSort.end());
+
+    // call mergeSort recursively
+    mergeSort(left);
+    mergeSort(right);
+
+    // merge back
+    int l = 0;
+    int r = 0;
+    int i = 0;
+
+    // Both arrays have unmerged elements
+    while (l < left.size() && r < right.size()) {
+        if (left[l] < right[r]) {
+            arrayToSort[i] = left[l];
+            i++;
+            l++;
+        } else {
+            arrayToSort[i] = right[r];
+            i++;
+            r++;
+        }
+    }
+
+    // One array has unmerged elements
+    while (l < left.size() || r < right.size()) {
+        if (l < left.size() && r >= right.size()) {
+            arrayToSort[i] = left[l];
+            i++;
+            l++;
+        }
+        if (r < right.size() && l >= left.size()) {
+            arrayToSort[i] = right[l];
+            i++;
+            r++;
+        }
+    }
+
+
 }
 
 /** 
  * quickSortHelper
- * Input: 
- * Output: 
- * The input array...
+ * Input: A vector of doubles to sort, and the index range that is sorted
+ * Output: none
+ * The input array between indices i and j are sorted.
  * 
  * Note that i is inclusive (i.e., is the first element of subarray).
  * Note that j is not inclusive (i.e., off the end of the subarray).
  * 
  */
 void quickSortHelper(vector<double> &arrayToSort, int i, int j) {
-    return;
+    // If the array has 1 element then it is sorted
+    if (j - i <= 1) {
+        return;
+    } else {
+        // Choose a pivot element value
+        double p = arrayToSort[i + (j - i) / 2];
+
+        // Partition the array based on the pivot
+        int a = i;
+        int b = j - 1;
+
+        while (a < b) {
+            // Move pointers inwards
+            while (arrayToSort[a] < p) {
+                a++;
+            }
+            while (arrayToSort[b] > p) {
+                b--;
+            }
+
+            // Swap when both pointers stuck
+            if (a <= b) {
+                swap(arrayToSort, a, b);
+                a++;
+                b--;
+            }
+        }
+
+        // Recursively perform quicksort on the partitioned sub-arrays
+        quickSortHelper(arrayToSort, i, b + 1);
+        quickSortHelper(arrayToSort, a, j);
+    }
 }
 
 /** 
  * quickSort
- * Input: 
- * Output: 
- * The input array...
+ * Input: A vector of doubles to sort
+ * Output: None
+ * This function makes a call to quickSortHelper from 0 to the size of the array
+ * which is the end. Thus it calls quickSortHelper on the entire array.
  */
 void quickSort(vector<double> &arrayToSort) {
     quickSortHelper(arrayToSort, 0, arrayToSort.size());
